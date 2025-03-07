10d13f4b3adbe78bb810a5e83be42cbb91979b25
Tony Liu <contacttonyliu@gmail.com>
2025-03-06 18:21:11 -0800

    rtd init

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,10 @@
 
 from pathlib import Path
 WORKSPACE_ROOT = Path(__file__).resolve().parents[2]
-print(Path(__file__))
+for p in Path(__file__).parents:
+    print(p)
+    for f in p.iterdir():
+        print(f"  - {f}")
 with open(WORKSPACE_ROOT/'src/metasmith/version.txt') as f:
     version_string = f.read().strip()
 
