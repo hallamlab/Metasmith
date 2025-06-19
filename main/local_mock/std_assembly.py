4db908047924f5e2fb82ad75e65ba56b5d7584ce
Sebastian Hyland <st.hyland05@gmail.com>
2025-06-18 18:41:49 -0700

    fix: ignore large datasets

--- main/local_mock/std_assembly.py
+++ main/local_mock/std_assembly.py
@@ -5,7 +5,7 @@ dtypes, containers, transforms = Std()
 base_dir = Path.cwd()
 
 
-path_to_agent_home = Path("./std_home")
+path_to_agent_home = Path("./std_home").resolve()
 smith = Agent(
     home = Source.FromLocal(path_to_agent_home),
 )
@@ -15,17 +15,15 @@ smith.Deploy()
 inputs = DataInstanceLibrary("std_assembly_data.xgdb")
 inputs.Add(
     items = [
-        (base_dir / "sample_data/accession_short", "fastq_short", "std::short_reads_accession"),
-        (base_dir / "sample_data/accession_long.fastq", "fastq_long", "std::long_reads_filtered"),
+        (base_dir / "sample_data/accession_short", "acc_short", "std::short_reads_accession"),
+        (base_dir / "sample_data/accession_long", "acc_long", "std::long_reads_accession"),
     ]
 )
 
 task = smith.GenerateWorkflow(
     given = [containers, inputs],
     transforms = [transforms],
-    targets = [dtypes["long_reads_assembly"]]
+    targets = [dtypes["hybrid_assembly"]]
 )
 smith.StageWorkflow(task, "clear")
 smith.RunWorkflow(task)
-
-smith.CheckWorkflow(task)
