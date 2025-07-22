c11fee782447718e86935cf370cba83acce323bb
Sebastian Hyland <st.hyland05@gmail.com>
2025-07-22 00:41:07 -0700

    feat: implement filtlong estimation based on miniasm output

---
+++ src/metasmith/std/transforms/filtlong_improved.py
@@ -0,0 +1,33 @@
+from metasmith.python_api import *
+
+lib = TransformInstanceLibrary.ResolveParentLibrary(__file__)
+model = Transform()
+
+reads      = model.AddRequirement(lib.GetType("std::long_reads"))
+estimate   = model.AddRequirement(lib.GetType("std::miniasm_estimate"))
+image      = model.AddRequirement(lib.GetType("std::oci_image_filtlong"))
+out        = model.AddProduct(lib.GetType("std::long_reads_filtered"))
+
+def protocol(context: ExecutionContext):
+    reads_path = context.Get(reads)
+    estimate_path = context.Get(estimate)
+    out_path = context.Get(out)
+    context.ExecWithContainer(
+        image = image,
+        cmd = f"""
+                filtlong \
+                    {reads_path.container} \
+                    --min_length 1000 \
+                    --target_bases $(< {estimate_path.container}) \
+                    > {out_path.container}
+        """
+    )
+    return ExecutionResult(success=out_path.local.exists())
+
+TransformInstance(
+    protocol = protocol,
+    model = model,
+    output_signature = {
+        out: "long_reads_filtered.fastq",
+    },
+)
--- src/metasmith/std/transforms/miniasm.py
+++ src/metasmith/std/transforms/miniasm.py
@@ -5,16 +5,11 @@ model = Transform()
 
 reads           = model.AddRequirement(lib.GetType("std::long_reads"))
 image_minimap2  = model.AddRequirement(lib.GetType("std::oci_image_minimap2"))
-mappings        = model.AddProduct(lib.GetType("std::long_reads_self_mappings"))
 image_miniasm   = model.AddRequirement(lib.GetType("std::oci_image_miniasm"))
-gfa             = model.AddProduct(lib.GetType("std::long_reads_gfa"))
-image_gfatools  = model.AddRequirement(lib.GetType("std::oci_image_gfatools"))
-out             = model.AddProduct(lib.GetType("std::long_reads_miniasm"))
+out             = model.AddProduct(lib.GetType("std::miniasm_estimate"))
 
 def protocol(context: ExecutionContext):
     reads_path = context.Get(reads)
-    mappings_path = context.Get(mappings)
-    gfa_path = context.Get(gfa)
     out_path = context.Get(out)
 
     cpus = context.params.get("cpus")
@@ -25,27 +20,25 @@ def protocol(context: ExecutionContext):
     context.ExecWithContainer(
         image = image_minimap2,
         cmd = f"""
+                cd {out_path.container.parent}
                 minimap2 \
                     -x ava-pb \
                     {cpus_string} \
-                    {reads_path.container} {reads_path.container} | gzip -1 > {mappings_path.container}
+                    {reads_path.container} {reads_path.container} | gzip -1 > self_mappings.gzip
         """
     )
     context.ExecWithContainer(
         image = image_miniasm,
         cmd = f"""
+                cd {out_path.container.parent}
                 miniasm \
                     -f {reads_path.container} \
-                    {mappings_path.container} \
-                    > {gfa_path.container}
-        """
-    )
-    context.ExecWithContainer(
-        image = image_gfatools,
-        cmd = f"""
-                gfatools gfa2fa \
-                    {gfa_path.container} \
-                    > {out_path.container}
+                    self_mappings.gzip \
+                    > miniasm.gfa
+
+                GENOME_SIZE=$(awk '/^S/ {{ sum += length($3) }} END {{ print sum }}' miniasm.gfa); \
+                TARGET_BASES=$(( GENOME_SIZE * 100 )); \
+                echo $TARGET_BASES > {out_path.container}
         """
     )
     return ExecutionResult(success=out_path.local.exists())
@@ -54,8 +47,6 @@ TransformInstance(
     protocol = protocol,
     model = model,
     output_signature = {
-        mappings: "mappings.paf.gz",
-        gfa: "reads.gfa",
-        out: "assembly.fasta"
+        out: "miniasm_estimate"
     },
 )
