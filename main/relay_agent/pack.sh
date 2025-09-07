e36fb8e14515288366e1fd31f2626384978ce67e
Tony Liu <contacttonyliu@gmail.com>
2025-09-06 19:43:42 -0700

    update relay build

--- main/relay_agent/pack.sh
+++ main/relay_agent/pack.sh
@@ -5,22 +5,30 @@ NAME=pyinstaller_build_env
 DOCKER_IMAGE=quay.io/txyliu/$NAME
 
 case $1 in
+    -x)
+        while true; do
+            sleep 1
+            echo x >>x
+        done
+    ;;
     -b)
-
         export DOCKER_BUILDKIT=1
         docker build \
             --build-arg="CONDA_ENV=${NAME}_env" \
             -t $DOCKER_IMAGE .
     ;;
-    -p)
-        # use docker to force older glibc version
+    -u)
         SRC=../../src/metasmith
         mkdir -p ./relay/coms
         rsync -auc $SRC/coms/ipc.py         ./relay/coms/ipc.py
         rsync -auc $SRC/serialization.py    ./relay/serialization.py
         rsync -auc $SRC/hashing.py          ./relay/hashing.py
         rsync -auc $SRC/logging.py          ./relay/logging.py
+    ;;
+    -p)
+        $HERE/pack.sh -u
         rm -r ./dist ./build
+        # use docker to force older glibc version
         docker run -it --rm \
             -u $(id -u):$(id -g) \
             --mount type=bind,source="$HERE",target="/ws"\
