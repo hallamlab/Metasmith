38d9d5bd32a81d2f19a414c395a9d03584d00e52
Tony Liu <contacttonyliu@gmail.com>
2025-10-24 10:21:24 -0700

    socketio server poc

---
+++ main/relay_agent/relay/server.py
@@ -0,0 +1,40 @@
+import os, sys
+from pathlib import Path
+from flask import Flask
+from flask_socketio import SocketIO, send, emit
+import gevent
+import signal
+
+app = Flask(__name__, static_url_path='', static_folder='public')
+
+HERE = Path(__file__).parent
+def getSecret():
+    secret_path = HERE/'secrets'
+    os.system(f'mkdir -p {secret_path}')
+    secret_path = secret_path/'secret'
+    try:
+        with open(secret_path, 'r') as s:
+            return s.readlines()[0][:-1]
+    except FileNotFoundError:
+        import secrets
+        with open(secret_path, 'w') as s:
+            tok = secrets.token_urlsafe(64)
+            s.write(tok)
+            s.flush()
+            return tok
+app.config['SECRET_KEY'] = getSecret()
+socketio = SocketIO(app, async_mode='gevent')
+def on_term_signal():
+    exit(0)
+gevent.signal_handler(signal.SIGTERM, on_term_signal)
+# @app.route('/')
+# def Home():
+#     return "hello"
+
+@socketio.on('echo')
+def handle_echo(data):
+    emit('echo', dict(echo=data), json=True)
+
+# URL_ROOT = 'http://localhost:12001'
+# API_PREFIX = 'api/v1'
+
